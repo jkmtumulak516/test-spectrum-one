979f472ed4d9c77a8496049b4f15785b1fd9ed7e
Jose Karol Tumulak <tumulakjkm@gmail.com>
2022-06-07 12:07:12 +0800

    created initial register user endpoint without email or activation key

--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,32 @@
-from django.contrib.auth import get_user_model
-from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from rest_framework.serializers import ModelSerializer, EmailField, CharField
+
 
 class UserSerializer(ModelSerializer):
+
+    email = EmailField()
+    password = CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
+    first_name = CharField(required=False)
+    last_name = CharField(required=False)
+
     class Meta:
-        model = get_user_model()
-        fields = ['id', 'email', 'first_name', 'last_name', 'is_active']
+        model = User
+        fields = ('id', 'email', 'first_name', 'last_name', 'is_active', 'password')
+        read_only_fields = ('id', 'is_active')
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        validated_data['username'] = validated_data['email']
+        validated_data['is_active'] = False
+        return super(UserSerializer, self).create(validated_data)
 
 class NoAuthUserSerializer(ModelSerializer):
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ['id', 'first_name', 'is_active']
+
--- users/views.py
+++ users/views.py
@@ -1,10 +1,31 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from users.serializers import UserSerializer
+from users import serializers
 
 # Create your views here.
 class UserViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
     permission_classes = []
+
+    @action(detail=False, methods=['post'], name='Register User')
+    def register(self, request):
+
+        serializer = serializers.UserSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
+
+        email = serializer.validated_data['email']
+        if User.objects.filter(email=email).exists():
+            return Response(f'{email} exists', status=400)
+
+        # TODO generate activation key
+        # TODO send email
+
+        serializer.save()
+
+        return Response(serializer.data, status=201)
